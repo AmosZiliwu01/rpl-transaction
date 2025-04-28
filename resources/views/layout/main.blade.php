b61c887195c71979867bdd0e33539290ee233481
AmosZiliwuUkrim <amos.aleksiato.z@mail.ukrim.ac.id>
2025-04-28 18:07:47 +0700

    tambah jquery

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -310,6 +310,8 @@
 <script src="{{asset('assets/dist/js/pages/dashboard.js')}}"></script>
 
 
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
